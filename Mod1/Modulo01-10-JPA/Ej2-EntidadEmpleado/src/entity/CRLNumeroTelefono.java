d163d132bb13d9bb4b6469e3304b796152c13967
Carlos Rodriguez Lopez <carlosrodriguezlopez82@gmail.com>
2014-12-27 13:03:28 +0100

    Ejercicio 2 de JPA finalizado

---
+++ Mod1/Modulo01-10-JPA/Ej2-EntidadEmpleado/src/entity/CRLNumeroTelefono.java
@@ -0,0 +1,77 @@
+package entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Version;
+
+@Entity
+@NamedQueries({ @NamedQuery(name = "CRLNumeroTelefono.findAll", query = "select o from CRLNumeroTelefono o") })
+public class CRLNumeroTelefono implements Serializable {
+    private static final long serialVersionUID = 7058414624314237005L;
+    @Id
+    private Integer id;
+    @Version
+    private Integer version;
+    @Column(name="prefijoInternacional", nullable = true)
+    private String prefijoInternacional;
+    @Column(name="numeroLocal", nullable = true)
+    private String numeroLocal;
+    @ManyToOne (fetch=FetchType.LAZY)
+    @JoinColumn(name="empleado_id")
+    private CRLEmpleado empleado;
+
+    public void setEmpleado(CRLEmpleado empleado) {
+        this.empleado = empleado;
+        if (!empleado.getCRLNumeroTelefono().contains(this)) {
+            empleado.getCRLNumeroTelefono().add(this);
+        }
+    }
+
+    public CRLEmpleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setPrefijoInternacional(String prefijoInternacional) {
+        this.prefijoInternacional = prefijoInternacional;
+    }
+
+    public String getPrefijoInternacional() {
+        return prefijoInternacional;
+    }
+
+    public void setNumeroLocal(String numeroLocal) {
+        this.numeroLocal = numeroLocal;
+    }
+
+    public String getNumeroLocal() {
+        return numeroLocal;
+    }
+    
+    public CRLNumeroTelefono() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+    
+}
---
+++ Mod1/Modulo01-10-JPA/Ej2-EntidadEmpleado/src/test/Startup.java
@@ -0,0 +1,103 @@
+package test;
+
+import entity.CRLEmpleado;
+import entity.CRLNumeroTelefono;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+
+public class Startup {
+    
+
+    public static void main(String[] args) throws ParseException {
+        Startup startup = new Startup();
+        startup.createData();
+        List<CRLEmpleado> empleados = startup.findAll();
+        for (CRLEmpleado e : empleados) {
+            System.out.println(e);
+            System.out.println(e.getId() +"--"+ e.getNombre()+"--"+e.getApellido()+"--"+e.getFechaalta()+"--"+e.getSalario());
+            HashSet <CRLNumeroTelefono> telfSet = (HashSet<CRLNumeroTelefono>) e.getCRLNumeroTelefono();
+            if (!telfSet.isEmpty()){
+                Iterator iterator = telfSet.iterator(); 
+                while (iterator.hasNext()){
+                    CRLNumeroTelefono telf = (CRLNumeroTelefono) iterator.next();
+                    System.out.println(telf.getEmpleado().getId() + "---" +  telf.getPrefijoInternacional() + " " + telf.getNumeroLocal());  
+                }
+            }
+        }
+        startup.cerrarConexiones();
+    }
+    
+    @PersistenceUnit
+    EntityManagerFactory emf;
+    EntityManager em;
+    public Startup() {
+        super();
+        emf = Persistence.createEntityManagerFactory("Empleado");
+        em = emf.createEntityManager();
+    }
+
+    private List<CRLEmpleado> findAll() {
+        Query q = em.createNamedQuery("CRLEmpleado.findAll");
+        return q.getResultList();
+    }
+
+    private void createData() throws ParseException {
+        CRLEmpleado e1, e2;
+        CRLNumeroTelefono n1a, n1b, n2;
+        Date date1, date2;
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        em.getTransaction().begin();
+        e1 = new CRLEmpleado();
+        e1.setId(1);
+        e1.setApellido("Rodriguez");
+        e1.setNombre("Carlos");
+        date1 = formatter.parse("2014-10-05");
+        e1.setFechaalta(date1);
+        e1.setSalario((long)150000000);
+        em.persist(e1);
+        n1a = new CRLNumeroTelefono();
+        n1a.setId(1);
+        n1a.setPrefijoInternacional("+34");
+        n1a.setNumeroLocal("636899504");
+        n1a.setEmpleado(e1);
+        em.persist(n1a);
+        n1b = new CRLNumeroTelefono();
+        n1b.setId(2);
+        n1b.setPrefijoInternacional("+35");
+        n1b.setNumeroLocal("6360000000");
+        n1b.setEmpleado(e1);
+        em.persist(n1b);
+        e2 = new CRLEmpleado();
+        e2.setId(2);
+        e2.setApellido("Ureña");
+        e2.setNombre("Macarena");
+        date2 = formatter.parse("2014-11-05");
+        e2.setFechaalta(date2);
+        e2.setSalario((long)250000000);
+        em.persist(e2);
+        n2 = new CRLNumeroTelefono();
+        n2.setId(3);
+        n2.setPrefijoInternacional("+38");
+        n2.setNumeroLocal("636884554");
+        n2.setEmpleado(e2);
+        em.persist(n2);
+        em.getTransaction().commit();
+    }
+
+    private void cerrarConexiones() {
+        em.close();
+        emf.close();
+    }
+}
